cf1c4ed1b2dcf5c166de76e49058b2382c06b098
Ryan Woods <contact@ryanofwoods.com>
2021-04-01 19:37:51 +0200

    cleanup files through eslint

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
