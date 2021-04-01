33b4576ba3fedced9d1c6c60174aa02fb862b844
Ryan Woods <contact@ryanofwoods.com>
2021-04-01 16:00:34 +0200

    add cities and citylist

---
+++ src/containers/city.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const City = ({ city }) => {
+  return (
+    <div>
+      {city.name}
+    </div>
+  );
+};
+
+export default City;
---
+++ src/containers/city_list.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions/index';
+
+import City from './city';
+
+class CityList extends Component {
+  static defaultProps = {
+    flats: [{ name: "Lyon", address: "23 Rue Paul Montrochet, 69002 Lyon", slug: "lyon" }]
+  }
+
+  componentWillMount() {
+    this.props.setCities();
+  }
+
+  render() {
+    const { cities } = this.props;
+    return (
+      <div>
+        {
+          cities.map((city) => {
+            return (<City city={city} />);
+          })
+        }
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setCities },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
